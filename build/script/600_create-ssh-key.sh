10d8da879e3ab9f021bd6577f3883ad87be57384
kesokaj <ostling.simon@gmail.com>
2024-11-07 10:37:23 +0100

    Otai1foyei1li1eixulughe1maetahxo

--- build/script/600_create-ssh-key.sh
+++ build/script/600_create-ssh-key.sh
@@ -2,4 +2,4 @@
 
 set -eoux pipefail
 
-runuser -l user -c "if [ ! -f /home/user/.ssh/id_ed25519 ]; then ssh-keygen -t ed25519 -C cloud-workstation-user -f /home/user/.ssh/id_ed25519; fi"
+runuser -l user -c "if [ ! -f /home/user/.ssh/id_ed25519 ]; then yes | ssh-keygen -t ed25519 -C cloud-workstation-user -f /home/user/.ssh/id_ed25519; fi"
